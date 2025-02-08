f6ec2344bcbea4e96c78e0259d221078236a32a5
Jonathan Anleu <jonathan.anleu@gmail.com>
2025-02-08 00:03:25 -0600

    Challenge of Consolas and Telim

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Challenge Consolas and Telim 
+/*Expected Output
+Bread is ready.
+Who is the bread for?
+RB
+Noted: RB got bread.*/ 
+
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Noted: {name} got bread.");
