db5e648fedcbd02a3d7718ec84cfffe2290c9e96
Jonathan Anleu <jonathan.anleu@gmail.com>
2025-02-08 13:48:23 -0600

    Buying Inventory challenge

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿// Repairing the Clocktower
-Console.Write("Enter a number for the clock: ");
-int clockNumber = Convert.ToInt32(Console.ReadLine());
-if (clockNumber % 2 == 0)
+﻿// Buying Inventory
+
+Console.Write("What number do you want to see the price of? ");
+string? response = Console.ReadLine();
+response = response switch 
 {
-  Console.WriteLine("Even");
-}else
-{
-  Console.WriteLine("Odd");
-}
+  "1" => "Rope: 10",
+  "2" => "Torches: 15",
+  "3" => "Climbing Equipment: 25",
+  "4" => "Clean Water: 1",
+  "5" => "Machete: 20",
+  "6" => "Canoe: 200",
+  "7" => "Food Supplies: 1",
+  _ => "Enter a number 1-7"
+};
+Console.WriteLine(response);
