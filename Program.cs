e72ab2a9dee79d42179503d0d4ad124dc16fc1ba
Jonathan Anleu <jonathan.anleu@gmail.com>
2025-02-08 20:03:10 -0600

    The Replicator of D'To

--- Program.cs
+++ Program.cs
@@ -1,26 +1,10 @@
-﻿// The Magic Cannon
+﻿// The Replicator of D'To
 
-int length = 100;
-for (int i = 1; i <= length; i++)
+int[] array = {5,3,7,4,6};
+int[] secondArray = new int[5];
+
+for(int i = 0; i < array.Length; i++)
 {
-  if (i % 3 == 0 && i % 5 == 0)
-  {
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine($"Combo Blast ({i})");
-  }
-  else if (i % 3 == 0)
-  {
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"Fire ({i})");
-  }
-  else if (i % 5 == 0)
-  {
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine($"Electric ({i})");
-  }
-  else
-  {
-    Console.ResetColor();
-    Console.WriteLine($"Normal ({i})");
-  }
+  secondArray[i] = array[i];
+  Console.WriteLine(secondArray[i]);
 }
--- obj/Debug/net9.0/psuedopad.AssemblyInfo.cs
+++ obj/Debug/net9.0/psuedopad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("psuedopad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c1005a2e5c7baa2d066d15e6f80a81866d1d41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+756c053401b3488022f7871ae6a7a475b1e6356b")]
 [assembly: System.Reflection.AssemblyProductAttribute("psuedopad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("psuedopad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
