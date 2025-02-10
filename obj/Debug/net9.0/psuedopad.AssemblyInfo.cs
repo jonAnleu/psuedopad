b829918b087d01924a31408b3272337c78c213b9
Jonathan Anleu <jonathan.anleu@gmail.com>
2025-02-10 00:45:55 -0600

    finished boss battle

--- obj/Debug/net9.0/psuedopad.AssemblyInfo.cs
+++ obj/Debug/net9.0/psuedopad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("psuedopad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9877e9b4f5b445a92f5deda6e250e3a7dfea7bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7f7faef11e8dd5827c6835ec38e39da9800080")]
 [assembly: System.Reflection.AssemblyProductAttribute("psuedopad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("psuedopad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
