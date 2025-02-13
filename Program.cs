cde2b49e2a654cdd067efdfe29e4ac0ccc939db0
Jonathan Anleu <jonathan.anleu@gmail.com>
2025-02-12 23:21:55 -0600

    The Point challenge

--- Program.cs
+++ Program.cs
@@ -1,113 +1,27 @@
-﻿//Arrow Factories (static methods)
-using System.ComponentModel.DataAnnotations;
+﻿// Boss Battle THE POINT 
 
-Console.WriteLine("What arrow do you want?");
-Console.WriteLine("1 - Elite Arrow");
-Console.WriteLine("2 - Beginner Arrow");
-Console.WriteLine("3 - Marksman Arrow");
-Console.WriteLine("4 - Custom Arrow");
+Point point = new(2, 3);
+Point point1= new(-4, 0);
+Point origin = new();
 
-string choice = Console.ReadLine();
+Console.WriteLine($"{origin.X} {origin.Y}");
+Console.WriteLine($"{point.X}, {point.Y}");
+Console.WriteLine($"{point1.X}, {point1.Y}");
 
-Arrow arrow = choice switch
+class Point
 {
-    "1" => Arrow.CreateEliteArrow(),
-    "2" => Arrow.CreateBeginnerArrow(),
-    "3" => Arrow.CreateMarksmanArrow(),
-    _ => CreateCustomArrow(),
-};
+  public int X { get; set; }
+  public int Y { get; set; }
 
-Console.WriteLine($"That arrow costs {arrow.Cost} gold.");
+  public Point(int x, int y)
+  {
+    X = x;
+    Y = y;
+  }
 
-
-
-Arrow CreateCustomArrow()
-{
-    Arrowhead arrowhead = GetArrowheadType();
-    Fletching fletching = GetFletchingType();
-    float length = GetLength();
-
-    return new Arrow(arrowhead, fletching, length);
+  public Point()
+  {
+    X = 0;
+    Y = 0;
+  }
 }
-
-Arrowhead GetArrowheadType()
-{
-    Console.Write("Arrowhead type (steel, wood, obsidian): ");
-    string input = Console.ReadLine();
-    return input switch
-    {
-        "steel" => Arrowhead.Steel,
-        "wood" => Arrowhead.Wood,
-        "obsidian" => Arrowhead.Obsidian
-    };
-}
-
-Fletching GetFletchingType()
-{
-    Console.Write("Fletching type (plastic, turkey feather, goose feather): ");
-    string input = Console.ReadLine();
-    return input switch
-    {
-        "plastic" => Fletching.Plastic,
-        "turkey feather" => Fletching.TurkeyFeathers,
-        "goose feather" => Fletching.GooseFeathers
-    };
-}
-
-float GetLength()
-{
-    float length = 0;
-
-    while (length < 60 || length > 100)
-    {
-        Console.Write("Arrow length (between 60 and 100): ");
-        length = Convert.ToSingle(Console.ReadLine());
-    }
-
-    return length;
-}
-
-public class Arrow
-{
-    public Arrowhead Arrowhead { get; }
-    public Fletching Fletching { get; }
-    public float Length { get; }
-
-    public Arrow(Arrowhead arrowhead, Fletching fletching, float length)
-    {
-        Arrowhead = arrowhead;
-        Fletching = fletching;
-        Length = length;
-    }
-
-    public float Cost
-    {
-        get
-        {
-            float arrowheadCost = Arrowhead switch
-            {
-                Arrowhead.Steel => 10,
-                Arrowhead.Wood => 3,
-                Arrowhead.Obsidian => 5
-            };
-
-            float fletchingCost = Fletching switch
-            {
-                Fletching.Plastic => 10,
-                Fletching.TurkeyFeathers => 5,
-                Fletching.GooseFeathers => 3
-            };
-
-            float shaftCost = 0.05f * Length;
-
-            return arrowheadCost + fletchingCost + shaftCost;
-        }
-    }
-
-    public static Arrow CreateEliteArrow() => new Arrow(Arrowhead.Steel, Fletching.Plastic, 95);
-    public static Arrow CreateBeginnerArrow() => new Arrow(Arrowhead.Wood, Fletching.GooseFeathers, 75);
-    public static Arrow CreateMarksmanArrow() => new Arrow(Arrowhead.Steel, Fletching.GooseFeathers, 65);
-}
-
-public enum Arrowhead { Steel, Wood, Obsidian }
-public enum Fletching { Plastic, TurkeyFeathers, GooseFeathers }
--- obj/Debug/net9.0/psuedopad.AssemblyInfo.cs
+++ obj/Debug/net9.0/psuedopad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("psuedopad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41bfc2302442b48171b0dda29456387001164312")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f4c1dffdd3668c8937f6bca64f2ac412e8a7f65")]
 [assembly: System.Reflection.AssemblyProductAttribute("psuedopad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("psuedopad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
